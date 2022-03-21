3c6adfae84a66a656d4f520a934c48b01d51bdab
Dejan Roshkovski <d.roshkovski@gmail.com>
2022-03-21 18:24:21 +0100

    Adding routes

--- config/routes.php
+++ config/routes.php
@@ -1,33 +1,37 @@
 <?php
 
-use Bramus\Router\Router;
 use Controllers\ExchangeController;
 use Controllers\PostsApiController;
 use Controllers\PostsController;
 
-$router = new Router();
-
-$router->get('/exchange', function() {
+checkRoute('GET', '/^\/calculate/', function() {
     ExchangeController::index();
 });
-$router->get('/api/posts/(\d+)', function($id) {
+
+checkRoute('GET', '/^\/api\/posts\/(\d+)/', function($id) {
     PostsApiController::show($id);
 });
-$router->get('/api/posts', function() {
+checkRoute('GET', '/^\/api\/posts/', function() {
     PostsApiController::index();
 });
-$router->post('/api/posts', function() {
+checkRoute('POST', '/^\/api\/posts/', function() {
     PostsApiController::create();
 });
-$router->post('/api/posts/edit/(\d+)', function($id) {
+checkRoute('POST', '/^\/api\/posts\/edit\/(\d+)/', function($id) {
     PostsApiController::edit($id);
 });
-$router->get('/posts', function() {
+checkRoute('GET', '/^\/posts/', function() {
     PostsController::getPosts();
 });
-$router->get('/posts/create', function() {
+checkRoute('GET', '/^\/posts\/create/', function() {
     PostsController::createPost();
 });
 
-
-$router->run();
+function checkRoute($method, $pattern, $callback){
+    if($method !== $_SERVER['REQUEST_METHOD']){
+        return 0;
+    }
+    if(preg_match_all($pattern, $_SERVER['REQUEST_URI'], $matches)){
+        $callback($matches);
+    }
+}
